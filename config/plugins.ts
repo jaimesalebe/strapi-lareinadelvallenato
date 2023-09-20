e4c09909af5833df44aea67c2892939c98b0327b
Jaime Salebe <jaimesalebe@outlook.com>
2023-09-20 14:28:29 -0500

    update strapi v4.13.7

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,24 @@
 module.exports = ({ env }) => ({
-    upload: {
-        config: {
-            provider: 'aws-s3',
-            providerOptions: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID', undefined),
-                secretAccessKey: env('AWS_ACCESS_SECRET', undefined),
-                region: env('AWS_REGION', undefined),
-                params: {
-                    ACL: env('AWS_ACL', 'public-read'),
-                    signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-                    Bucket: env('AWS_BUCKET'),
-                },
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
-        },
-    },
-});
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID', undefined),
+          secretAccessKey: env('AWS_ACCESS_SECRET', undefined),
+          region: env('AWS_REGION', undefined),
+          params: {
+            ACL: env('AWS_ACL', 'public-read'),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('AWS_BUCKET')
+          }
+        }
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {}
+      }
+    }
+  }
+})
